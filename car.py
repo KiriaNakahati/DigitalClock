bdc940f7f5c0e20e5971083cb70e9aa5da8053b6
Kiria Nakahati <kirianakahati@outlook.com>
2025-10-22 17:10:38 -0300

    finished

---
+++ car.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+        
+stu1 = Student("Bob", 34)
+stu2 = Student("patrick", 45)
+
+print()
---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+import datetime
+import pygame 
+
+def set_alarm(alarm_time):
+    print(f" alarm set for {alarm_time}")
+    sound_file = "my_music.mp3"
+    is_running = True 
+
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+        if current_time == alarm_time:
+            print("wake up")
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+    
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+            is_running = False
+    
+        time.sleep(1)
+
+
+
+alarm = input("enter the alarm time (H:M:S): ")
+set_alarm(alarm)
+
